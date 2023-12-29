3d2558a35b107c7237c23e8852353faad65984ec
NeptuneGo <NeptuneGo@hotmail.com>
2023-12-29 17:56:07 +0800

    fixed aavepool v2 convert

--- contracts/subStrategies/aavePool/aavePoolV2.sol
+++ contracts/subStrategies/aavePool/aavePoolV2.sol
@@ -18,7 +18,7 @@ contract aavePoolV2 {
         uint256[] memory prices = IAaveOracle(aaveOracle).getAssetsPrices(tokens);
         uint8 decimalsIn = IERC20Metadata(_tokenIn).decimals();
         uint8 decimalsOut = IERC20Metadata(_tokenOut).decimals();
-        return _amount*prices[0]*(10**decimalsIn)/(10**decimalsOut)/prices[1];
+        return _amount*prices[0]*(10**decimalsOut)/(10**decimalsIn)/prices[1];
     }
     function getCollateral(address _user) external view returns (uint256) {
         (uint256 c, , , , , ) = IAave(aave).getUserAccountData(_user);
